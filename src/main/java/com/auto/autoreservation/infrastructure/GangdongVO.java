23178affd58b719c95113553fed1f116354a902e
momo <jmjm9797@naver.com>
2024-07-05 18:47:29 +0900

    add : 확인 불필요한 날짜는 확인하지 않기위해 날짜 추가

--- src/main/java/com/auto/autoreservation/infrastructure/GangdongVO.java
+++ src/main/java/com/auto/autoreservation/infrastructure/GangdongVO.java
@@ -13,6 +13,7 @@ public class GangdongVO {
     private int year;
     private int month;
     private String time;
+    private String date;
     private List<String> day;
 
     // Getters and Setters
